614e77c21b4e9c5e087e93ae08df97309a0d1f23
Bruno Bravo <bruno.bravo@sprint.com>
2019-05-26 15:36:03 -0300

    fixing app prefix

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('gba-root h1')).getText() as Promise<string>;
+    return element(by.css('nga-root h1')).getText() as Promise<string>;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'gba-root',
+  selector: 'nga-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
